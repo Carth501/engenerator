e57c393a37c28ab2a624997b9905d6065cbdd6b0
Carth501 <carth501@gmail.com>
2024-07-08 17:56:13 -0400

    add random seed button

--- src/content/SeedControls.jsx
+++ src/content/SeedControls.jsx
@@ -1,16 +1,26 @@
+import { Button } from '@mui/material';
 import Input from '@mui/material/Input';
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
+import dice from '../images/casino_dice.svg';
 import { getOptions } from '../redux/selectors';
+import './SeedControls.css';
 
 export const SeedControls = () => {
+    const [seedString, setSeedString] = useState('')
     const options = useSelector(
         getOptions
     )
 
     const dispatch = useDispatch();
 
+    function randomSeed() {
+        const newSeed = Math.floor(Math.random() * 4294967296).toString();
+        updateInput(newSeed);
+    }
+
     function updateInput(input) {
+        setSeedString(input);
         dispatch({
             type: 'RUN_SHOP_GENERATE',
             payload: {
@@ -21,10 +31,19 @@ export const SeedControls = () => {
     };
 
     return (
-        <Input
-            type="text"
-            id="seed"
-            placeholder='seed'
-            onChange={e => updateInput(e.target.value)} />
+        <div className='seed-controls'>
+            <Button
+                color='secondary'
+                className='random-seed-button'
+                onClick={e => randomSeed()}>
+                <img src={dice} alt="randomize seed" className='dice-icon' />
+            </Button>
+            <Input
+                type="text"
+                id="seed"
+                placeholder='seed'
+                value={seedString}
+                onChange={e => updateInput(e.target.value)} />
+        </div>
     )
 }
