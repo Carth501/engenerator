3484832d3d3f24c74c259a69c90588e57373d699
Carth501 <carth501@gmail.com>
2024-07-09 20:41:19 -0400

    add tabgroup for additional pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
+import Box from '@mui/material/Box';
 import { ThemeProvider } from '@mui/material/styles';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 import './App.css';
 import { SeedControls } from './content/SeedControls.jsx';
 import ShopPage from './content/shop/ShopPage.jsx';
@@ -6,8 +11,43 @@ import cLogo from './images/cLogo.png';
 import engenLogo from './images/engeneratorLogo.png';
 import { engen_theme } from './theme';
 
+function CustomTabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+        </div>
+    );
+}
+
+CustomTabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
 
 function App() {
+    const [activeTab, setActiveTab] = useState('shops')
+
+
+    const handleChange = (event, newValue) => {
+        setActiveTab(newValue);
+    };
+
     return (
         <div className="App">
             <ThemeProvider theme={engen_theme}>
@@ -16,7 +56,22 @@ function App() {
                     <SeedControls />
                 </div>
                 <div className='page'>
-                    <ShopPage />
+                    <Box sx={{ borderBottom: 2, borderColor: 'divider' }}>
+                        <Tabs
+                            value={activeTab}
+                            onChange={handleChange}
+                            aria-label="basic tabs example"
+                            textColor="secondary">
+                            <Tab label="Shops" {...a11yProps(0)} />
+                            <Tab label="Characters" {...a11yProps(1)} />
+                        </Tabs>
+                    </Box>
+                    <CustomTabPanel value={activeTab} index={0}>
+                        <ShopPage />
+                    </CustomTabPanel>
+                    <CustomTabPanel value={activeTab} index={1}>
+                        Characters
+                    </CustomTabPanel>
                 </div>
                 <footer className='App-footer'>
                     <img src={cLogo} alt="site by C" className='designer-logo' />
